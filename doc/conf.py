840160f28a3987bbe7e05d6a60bdf4d6ba5d1a3f
marcosf2 <marcosf2@illinois.edu>
2026-01-23 16:47:22 -0600

    minor fixes

--- doc/conf.py
+++ doc/conf.py
@@ -30,6 +30,7 @@ language = "en"
 html_theme = "pydata_sphinx_theme"
 html_static_path = ['_static']
 html_logo = '_static/logo.png'
+html_baseurl = 'https://toolsforexperiments.github.io/toolsforexperiments/'
 
 html_theme_options = {
     "logo": {
